5db66ec557afba1e41fdfc25435daa0d9017751d
Darshana Perera <ds.perera.1997@gmail.com>
2026-04-07 16:16:03 +0530

    live backend added

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -6,4 +6,5 @@
  *
  * To use Create React App dev proxy instead (package.json "proxy"), set to ''.
  */
-export const API_BASE_URL = 'http://localhost:369';
+export const API_BASE_URL = 'http://69.197.187.24:369';
+// export const API_BASE_URL = 'http://localhost:369';
